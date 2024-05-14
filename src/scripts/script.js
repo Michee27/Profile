821a4d7fc0cbd512edb7f7620a0c2dbc09b99d2a
Michée Celestin <micheec@yahoo.fr>
2024-05-13 21:15:18 -0300

    Melhorando portfolio

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,6 +5,23 @@ function animateButtonClick(button) {
     }, 300);
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('.cabecalho-menu-link');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
+
 document.getElementById('menu-toggle').addEventListener('click', function () {
     document.getElementById('menu-overlay').classList.toggle('active');
 });
